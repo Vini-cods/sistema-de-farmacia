512859d02463c6563acf71f96775f2fde5e72661
Vini-cods <vinisanto141@gmail.com>
2025-07-23 01:49:03 -0300

    Refactor Main class to initialize Item objects and register them in BancoDeDados

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,17 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.controller.BancoDeDados;
+import org.example.model.Item;
+
 public class Main {
     public static void main(String[] args) {
 
+        Item dipirona = new Item("Dipirona 500mg", 100, "Caixa");
+        Item bromoprida = new Item( "Bromoprida 200mg", 50, "Caixa");
+
+        BancoDeDados banco = new BancoDeDados();
+        banco.cadastrar(dipirona);
+        banco.cadastrar(bromoprida);
 
 
     }
--- src/main/java/org/example/controller/BancoDeDados.java
+++ src/main/java/org/example/controller/BancoDeDados.java
@@ -1,4 +1,29 @@
 package org.example.controller;
 
+import org.example.model.Item;
+
 public class BancoDeDados {
+
+    private Item item;
+
+    public BancoDeDados() {
+
+    }
+
+    public void cadastrar(Item item) {
+
+    }
+
+    public void editar(int codigo) {
+
+    }
+
+    public void pesquisar(int codigo) {
+
+    }
+
+    public void excluir(int codigo) {
+
+    }
+
 }
